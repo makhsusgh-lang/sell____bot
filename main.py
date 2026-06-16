51c8c62af0003aac802f337d76d9b6d5ca53aaee
makhsusgh-lang <makhsusgh@gmail.com>
2026-06-16 05:37:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ import database as db
 # ---------------------------------------------------------------------------
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8589038863:AAFBrmmLV5KOcO0Mw8PCGU0F8_KQv32GR_U")
-ADMIN_GROUP_ID = int(os.environ.get("ADMIN_GROUP_ID", "-1004484919863"))
-ADMIN_ID = int(os.environ.get("ADMIN_ID", "997824630"))
+ADMIN_GROUP_ID = int(os.environ.get("ADMIN_GROUP_ID", "-1003966066313"))
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "8589038863"))
 SUPPORT_USERNAME = os.environ.get("SUPPORT_USERNAME", "Tm_mtk")
 
 logging.basicConfig(
